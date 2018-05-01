9c96e3c9d628ad3740e5e312ac61af15ecd72e58
junxuelian <917136891@qq.com>
2018-05-01 11:20:04 +0800

    add annoations

---
+++ src/main/java/com/mmall/concurrency/annoations/NotRecommend.java
@@ -0,0 +1,21 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by chenjun on 2018/5/1.
+ */
+
+/**
+ * 标记【不推荐】的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+
+    String value() default "";
+}
+
---
+++ src/main/java/com/mmall/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,21 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by chenjun on 2018/5/1.
+ */
+
+/**
+ * 标记【线程不安全】的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+
+    String value() default "";
+}
+
